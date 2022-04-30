53f47b2295efd7c1fc5b4c66bc10f6e29af32aba
Maxpittock <maxpittock5@gmail.com>
2022-04-30 11:51:06 +0100

    First Commit

--- src/commanding_velocity/scripts/Assignment.py
+++ src/commanding_velocity/scripts/Assignment.py
@@ -11,11 +11,12 @@ class MoveColour:
         #bridge for converting ensor data to open cv image
         self.bridge = CvBridge()
         #subscriber for gathering the sensor image data from the robot
-        self.image_sub = rospy.Subscriber("/image_raw",Image, self.visualise_colours)
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image, self.visualise_colours)
 
-        print("")
+        print("intialising")
 
     def visualise_colours(self, data):
+
         #create the image window
         namedWindow("Image window")
         #Convert the sensor data to op[en cv imge using the bridge and store in variable
@@ -28,36 +29,65 @@ class MoveColour:
         hsv_img = cvtColor(cv_image, COLOR_BGR2HSV)
 
         #numpy arry for colour ranges 
-        #green
-        green = numpy.array([0, 255, 0])
-        dark_green = numpy.array([0, 128, 0])
-        #yellow
-        lower_yellow = numpy.array([10, 60, 170])
-        upper_yellow = numpy.array([255, 255, 255])
-        #blue
-        blue = numpy.array([255, 0, 0])
-        darkblue = numpy.array([128, 0, 0])
         #red
         red = numpy.array([0, 0, 255])
         dark_red = numpy.array([0, 0, 128])
+        #green
+        green = numpy.array([0, 255, 0])
+        dark_green = numpy.array([0, 128, 0])
+        #blue
+        blue = numpy.array([255, 0, 0])
+        darkblue = numpy.array([128, 0, 0])
+        #yellow
+        lower_yellow = numpy.array([10, 60, 170])
+        upper_yellow = numpy.array([255, 255, 255])
+
 
         #crete  mssk lookign for colours between the ranges given
         identify_red = inRange(hsv_img, red, dark_red)
+        identify_green = inRange(hsv_img, green, dark_green)
+        identify_blue = inRange(hsv_img, blue, darkblue)
+        identify_yellow = inRange(hsv_img, upper_yellow, lower_yellow)
+
 
         #finds the contours within the range given
         _, red_hsv_contours, hierachy = findContours(identify_red.copy(),RETR_TREE,CHAIN_APPROX_SIMPLE)
+        _, green_hsv_contours, hierachy = findContours(identify_green.copy(),RETR_TREE,CHAIN_APPROX_SIMPLE)
+        _, blue_hsv_contours, hierachy = findContours(identify_blue.copy(),RETR_TREE,CHAIN_APPROX_SIMPLE)
+        _, yellow_hsv_contours, hierachy = findContours(identify_yellow.copy(),RETR_TREE,CHAIN_APPROX_SIMPLE)
 
         for c in red_hsv_contours:
             # This allows to compute the area (in pixels) of a contour
             a = cv2.contourArea(c)
             # and if the area is big enough, we draw the outline of the contour (in blue)
-            if a > 100.0:
-                cv2.drawContours(cv_image, c, -1, (255, 0, 0), 3)
+            #if a > 100.0:
+            cv2.drawContours(cv_image, c, -1, (255, 0, 0), 3)
+
+        for c in green_hsv_contours:
+            # This allows to compute the area (in pixels) of a contour
+            a = cv2.contourArea(c)
+            # and if the area is big enough, we draw the outline of the contour (in blue)
+            #if a > 100.0:
+            cv2.drawContours(cv_image, c, -1, (255, 0, 0), 3)
+        
+        for c in blue_hsv_contours:
+            # This allows to compute the area (in pixels) of a contour
+            a = cv2.contourArea(c)
+            # and if the area is big enough, we draw the outline of the contour (in blue)
+            #if a > 100.0:
+            cv2.drawContours(cv_image, c, -1, (255, 0, 0), 3)
+        
+        for c in yellow_hsv_contours:
+            # This allows to compute the area (in pixels) of a contour
+            a = cv2.contourArea(c)
+            # and if the area is big enough, we draw the outline of the contour (in blue)
+            #if a > 100.0:
+            cv2.drawContours(cv_image, c, -1, (255, 0, 0), 3)
 
         #display the image window with the open cv image
         imshow("Image window", cv_image)
         waitKey(1)
 
-rospy.init_node('MoveColour', anonymous=True)
-MoveColour()
+rospy.init_node('run', anonymous=True)
+run = MoveColour()
 rospy.spin()
